5f651f81d5a82be187dd4a5b8fdc6a8a6e5fb2f6
Nao Xof <naoxof@gmail.com>
2019-07-12 22:48:39 +0900

    Initial Commit

---
+++ collectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collectionView
+//
+//  Created by Yoshinori Kasaya on 2019/07/12.
+//  Copyright © 2019 NaoXof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
